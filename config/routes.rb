de627973e1a8fc620decd1c8799f2c5ace06cf02
Bryan H <bbharned@gmail.com>
2016-05-23 19:03:45 +0000

    add UI for new users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   
   root 'pages#index'
   get 'about', to: 'pages#about'
+
   
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   
   
   # The priority is based upon order of creation: first created -> highest priority.
